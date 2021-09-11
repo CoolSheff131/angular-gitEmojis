eaac2f1bbc33c6ed08a6536d917c5fe4afba88dc
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-11 14:02:49 +0800

    sidebar вынесен в отдельный компонент

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ConfigService } from './config/config.service';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
