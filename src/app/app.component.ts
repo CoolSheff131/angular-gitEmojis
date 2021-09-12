c5fc408d0ddf5ea73bcfe5c45260beb21205aa1a
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-12 17:06:18 +0800

    Добавлено сохранение состояния списков

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ConfigService } from './config/config.service';
 import { category, emoji } from './config/interfaces';
 import { EmojiTableComponent } from './emoji-table/emoji-table.component';
@@ -22,20 +22,44 @@ export class AppComponent implements OnInit{
   allemojis:emoji[] =[]
   favemojis:emoji[] =[]
   delemojis:emoji[] =[]
-  itemsPerPage:number = 3
+  itemsPerPage:number = 7
   @ViewChild(EmojiTableComponent)  emojiTable!: EmojiTableComponent
   public collectionSize: number
 
+  loadLists(){
+    let jsonArr = localStorage.getItem("deleted")
+    if(jsonArr){
+      this.delemojis = JSON.parse(jsonArr);
+      console.log("del loaded");
+      
+    }else console.log("not loaded");
+    
+    jsonArr = localStorage.getItem("favorite")
+    if(jsonArr){
+      this.favemojis = JSON.parse(jsonArr)
+      console.log("fav loaded");
+      
+    }
+
+    
+  }
+
+  saveLists(){
+    localStorage.setItem("deleted",JSON.stringify(this.delemojis))
+    localStorage.setItem("favorite",JSON.stringify(this.favemojis))
+  }
 
   constructor(private configService: ConfigService){
     this.collectionSize=1
   }
+  
 
   ngOnInit(): void {
     this.configService.getConfig().subscribe((arr)=>{
       this.allemojis = arr
       this.showCategory(0)
-    }) 
+    })
+    this.loadLists()
   }
 
   showCategory(index: number) { 
@@ -72,7 +96,7 @@ export class AppComponent implements OnInit{
     this.favemojis.push(emoji)
   }
   del(index: number){
-    
+
     let emoji = this.allemojis[index]
     emoji.isDeleted = !emoji.isDeleted
     this.allemojis.splice(index,1)
